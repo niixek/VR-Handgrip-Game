eba90ad8d0298446be696985ec837a8472c740db
Kexin Chen <kchen9988@me.com>
2021-03-14 01:45:49 -0500

    Updated Squeezable
    Didn't add much

--- Assets/Squeezable.cs
+++ Assets/Squeezable.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Squeezable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject destroyedVersion;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
