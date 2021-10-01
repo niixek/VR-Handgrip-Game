c86666d85448a11eece6655de1b7d1065885c1db
Kexin Chen <kchen9988@me.com>
2021-10-01 00:33:03 -0400

    Grip sliders change color based on value

---
+++ Assets/SliderColorChanger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderColorChanger : MonoBehaviour
+{
+    private Slider slider;
+
+    public int maxGrip = 100;
+    public Image fill;
+
+    public Color maxHealthColor = Color.red;
+    public Color minHealthColor = Color.green;
+
+    private void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        slider.minValue = 0f;
+        slider.maxValue = maxGrip;
+        slider.value = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fill.color = Color.Lerp(minHealthColor, maxHealthColor, (float)slider.value / maxGrip);
+        Debug.Log(slider.value);
+    }
+}
