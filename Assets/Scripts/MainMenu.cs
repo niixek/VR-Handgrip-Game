94af7d24d2b1e08b1b3d636b239a8a7ec7311d3c
Kexin Chen <kchen9988@me.com>
2021-11-07 00:50:51 -0400

    Main menu works now

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("play");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
